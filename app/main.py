c40c1e062f380236cdd2ab000481ed5bca47d2f1
priyanka Tripathi <priyankatripathi0228@gmail.com>
2026-02-23 01:01:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from app.orchestrator import Orchestrator
 
+def main():
+    repo_url = input("Enter GitHub repository URL: ")
+    
+    orchestrator = Orchestrator(repo_url)
+    orchestrator.run()
+
+if __name__ == "__main__":
+    main()
